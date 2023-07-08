5e707b8eb5a041d914012711ed85ea41299cb2ac
ksy3432 <120364372+ksy3432@users.noreply.github.com>
2023-07-08 22:11:18 +0900

    All styles applied successfully

--- src/components/NweetFactory.js
+++ src/components/NweetFactory.js
@@ -4,6 +4,8 @@ import { v4 } from "uuid";
 import { addDoc, collection} from "@firebase/firestore";
 import { dbService, storageService } from "fbase";
 import { useRef, useState} from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus, faTimes } from "@fortawesome/free-solid-svg-icons";
 
 const NweetFactory = ({ userObj }) => {
     const [nweet, setNweet] = useState("");
@@ -35,7 +37,7 @@ const NweetFactory = ({ userObj }) => {
         await addDoc(collection(dbService,"nweets"),nweetObj);
         setNweet("");
         setAttachment("");
-        fileInput.current.value = null;
+        fileInput.current = null;
         };
 
         const onChange = (event) => {
@@ -63,22 +65,51 @@ const NweetFactory = ({ userObj }) => {
 
     const onClearAttachment = () => {
         setAttachment("");
-        fileInput.current.value = null;
+        fileInput.current = null;
     };
 
     return (
-        <form onSubmit={onSubmit}>
-                <input value={nweet} onChange={onChange} type="text" placeholder="What's on your mind?" maxLength={120} />
-                <input type="file" accept="image/*" onChange={onFileChange} ref={fileInput} />
-                <input type="submit" value="nweet" />
-                {attachment && (
-                <div>
-                    <img src={attachment} width="50px" height="50px" />
-                    <button onClick={onClearAttachment}>Clear Image</button>                
+        <form onSubmit={onSubmit} className="factoryForm">
+          <div className="factoryInput__container">
+            <input
+              className="factoryInput__input"
+              value={nweet}
+              onChange={onChange}
+              type="text"
+              placeholder="What's on your mind?"
+              maxLength={120}
+            />
+            <input type="submit" value="&rarr;" className="factoryInput__arrow" />
+          </div>
+          <label for="attach-file" className="factoryInput__label">
+            <span>Add photos</span>
+            <FontAwesomeIcon icon={faPlus} />
+          </label>
+          <input
+            id="attach-file"
+            type="file"
+            accept="image/*"
+            onChange={onFileChange}
+            style={{
+              opacity: 0,
+            }}
+          />
+          {attachment && (
+            <div className="factoryForm__attachment">
+              <img
+                src={attachment}
+                style={{
+                  backgroundImage: attachment,
+                }}
+              />
+              <div className="factoryForm__clear" onClick={onClearAttachment}>
+                <span>Remove</span>
+                <FontAwesomeIcon icon={faTimes} />
+              </div>
             </div>
-                )}
-        </form>  
-    );
-}
+          )}
+        </form>
+      );
+    };
 
 export default NweetFactory;
--- src/routes/Home.js
+++ src/routes/Home.js
@@ -23,9 +23,9 @@ export default ({ userObj }) => {
     }, []);
 
     return (
-        <div>
+        <div className="container">
             <NweetFactory userObj={userObj} />  
-            <div>
+            <div style={{ marginTop: 30 }}>
                 {nweets.map((nweet) => (
                 <Nweet key={nweet.id}
                         nweetObj={nweet} 
